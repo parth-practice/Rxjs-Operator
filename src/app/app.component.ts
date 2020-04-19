934406a871eaced0bf1d7a1a8e14f6e29bd64cfc
Parth Patel <parth_patel@comcast.com>
2020-04-19 18:55:01 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Rxjs-Operator';
+export class AppComponent implements OnInit {
+
+  public ngOnInit(): void {
+    console.log("This is Test");
+  }
 }
